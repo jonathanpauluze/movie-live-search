8cfe3f8ac0877fc1a1b4a8363d220028638f910d
Jonathan Pauluze <jonathanpauluze@gmail.com>
2025-07-14 15:59:06 -0300

    chore: setting aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+
+const dirname =
+  typeof __dirname !== 'undefined'
+    ? __dirname
+    : path.dirname(fileURLToPath(import.meta.url))
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -7,5 +14,13 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     port: 5173
+  },
+  resolve: {
+    alias: [
+      {
+        find: '@',
+        replacement: path.resolve(dirname, './src')
+      }
+    ]
   }
 })
