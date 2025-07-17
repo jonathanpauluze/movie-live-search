27cee3e26291dc794b25c4be155af177b6350423
Jonathan Pauluze <jonathanpauluze@gmail.com>
2025-07-17 17:45:49 -0300

    feat: creating intersection observer hook

---
+++ src/hooks/use-intersection-observer.ts
@@ -0,0 +1,27 @@
+import { useEffect, useRef } from 'react'
+
+export function useIntersectionObserver<T extends Element = HTMLElement>(
+  callback: () => void,
+  options?: IntersectionObserverInit
+) {
+  const observerRef = useRef<T | null>(null)
+
+  useEffect(() => {
+    const target = observerRef.current
+    if (!target) return
+
+    const observer = new IntersectionObserver(([entry]) => {
+      if (entry.isIntersecting) {
+        callback()
+      }
+    }, options)
+
+    observer.observe(target)
+
+    return () => {
+      observer.disconnect()
+    }
+  }, [callback, options])
+
+  return observerRef
+}
