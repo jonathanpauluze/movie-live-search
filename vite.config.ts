ad63f8a528ac5b986e3035837c495ae589810d75
Jonathan Pauluze <jonathanpauluze@gmail.com>
2025-07-18 16:44:29 -0300

    chore: setting up vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 import path from 'node:path'
 import { fileURLToPath } from 'node:url'
@@ -14,6 +14,24 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     port: 5173
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './vitest.setup.ts',
+    include: ['src/**/*.test.{ts,tsx}'],
+    coverage: {
+      provider: 'v8',
+      reportsDirectory: 'coverage',
+      reporter: ['text', 'html'],
+      exclude: ['node_modules', 'src/main.ts'],
+      include: [
+        'src/components/**/*.{tsx,ts}',
+        'src/hooks/**/*.{tsx,ts}',
+        'src/utils/**/*.{tsx,ts}',
+        'src/pages/**/*.{tsx,ts}'
+      ]
+    }
   },
   resolve: {
     alias: [
