b4c6d69791d0a63e0fdd0634045af3e6e8e145c9
Jonathan Pauluze <jonathanpauluze@gmail.com>
2025-07-20 13:14:09 -0300

    test(movie-search-bar): adding more tests

--- src/components/movie-search-bar/movie-search-bar.test.tsx
+++ src/components/movie-search-bar/movie-search-bar.test.tsx
@@ -2,50 +2,78 @@ import { render, screen, fireEvent, cleanup } from '@testing-library/react'
 import { MovieSearchBar } from '.'
 import { vi } from 'vitest'
 
+const toggleMock = vi.fn()
+const hasMock = vi.fn(() => false)
+
 vi.mock('@/hooks/use-click-outside', () => ({
   useClickOutside: vi.fn()
 }))
 
 vi.mock('@/hooks/use-favorites', () => ({
   useFavorites: () => ({
-    toggle: vi.fn()
+    toggle: toggleMock,
+    has: hasMock
   })
 }))
 
-const suggestions = [
-  {
-    id: 123,
-    title: 'Clube da Luta',
-    genres: ['Ação'],
-    poster: '/poster.jpg',
-    release_year: 1999
-  },
-  {
-    id: 124,
-    title: 'Clube da Luta 2: Mais luta ainda',
-    genres: ['Ação'],
-    poster: '/poster.jpg',
-    release_year: 2025
+function renderMovieSearchBar(search = '') {
+  const handleChange = vi.fn()
+  const handleSelect = vi.fn()
+
+  const suggestions = [
+    {
+      id: 123,
+      title: 'Clube da Luta',
+      genres: ['Ação'],
+      poster: '/poster.jpg',
+      release_year: 1999
+    },
+    {
+      id: 124,
+      title: 'Clube da Luta 2: Mais luta ainda',
+      genres: ['Ação'],
+      poster: '/poster.jpg',
+      release_year: 2025
+    }
+  ]
+
+  const renderResult = render(
+    <MovieSearchBar
+      value={search}
+      onChange={handleChange}
+      onSelect={handleSelect}
+      suggestions={suggestions}
+    />
+  )
+
+  return {
+    handleChange,
+    handleSelect,
+    suggestions,
+    renderResult
   }
-]
+}
 
 describe('MovieSearchBar component', () => {
+  beforeAll(() => {
+    Element.prototype.scrollIntoView = vi.fn()
+
+    globalThis.IntersectionObserver = function () {
+      return {
+        observe: () => undefined,
+        unobserve: () => undefined,
+        disconnect: () => undefined,
+        takeRecords: () => []
+      }
+    } as unknown as typeof IntersectionObserver
+  })
+
   afterEach(() => {
     cleanup()
   })
 
   it('renders input and suggestions', () => {
-    const handleChange = vi.fn()
-    const handleSelect = vi.fn()
-
-    render(
-      <MovieSearchBar
-        value=""
-        onChange={handleChange}
-        onSelect={handleSelect}
-        suggestions={suggestions}
-      />
-    )
+    renderMovieSearchBar()
 
     const input = screen.getByRole('textbox')
     expect(input).toBeInTheDocument()
@@ -53,37 +81,39 @@ describe('MovieSearchBar component', () => {
   })
 
   it('shows autocomplete suggestion when typing matches', async () => {
-    screen.debug()
-
-    const handleChange = vi.fn()
-    const handleSelect = vi.fn()
-
-    render(
-      <MovieSearchBar
-        value="Clube"
-        onChange={handleChange}
-        onSelect={handleSelect}
-        suggestions={suggestions}
-      />
-    )
+    renderMovieSearchBar('Clube')
 
     expect(
       screen.getByText('da Luta - Utilize a tecla → para aceitar a sugestão')
     ).toBeInTheDocument()
   })
 
-  it('accepts suggestion with ArrowRight', () => {
-    const handleChange = vi.fn()
-    const handleSelect = vi.fn()
+  it('navigates with ArrowDown and ArrowUp between suggestions', () => {
+    const { handleSelect, suggestions } = renderMovieSearchBar('Clube')
 
-    render(
-      <MovieSearchBar
-        value="Clube"
-        onChange={handleChange}
-        onSelect={handleSelect}
-        suggestions={suggestions}
-      />
-    )
+    const input = screen.getByRole('textbox')
+
+    fireEvent.keyDown(input, { key: 'ArrowDown' })
+    fireEvent.keyDown(input, { key: 'ArrowDown' })
+    fireEvent.keyDown(input, { key: 'ArrowUp' })
+    fireEvent.keyDown(input, { key: 'Enter' })
+
+    expect(handleSelect).toHaveBeenCalledWith(suggestions[0])
+  })
+
+  it('calls toggle function when press Space in highlighted suggestion', () => {
+    const { suggestions } = renderMovieSearchBar('Clube')
+
+    const input = screen.getByRole('textbox')
+
+    fireEvent.keyDown(input, { key: 'ArrowDown' })
+    fireEvent.keyDown(input, { key: ' ' })
+
+    expect(toggleMock).toHaveBeenCalledWith(suggestions[0])
+  })
+
+  it('accepts suggestion with ArrowRight', () => {
+    const { handleChange } = renderMovieSearchBar('Clube')
 
     const input = screen.getByRole('textbox')
 
@@ -93,23 +123,45 @@ describe('MovieSearchBar component', () => {
     expect(handleChange).toHaveBeenCalledWith('Clube da Luta')
   })
 
-  it('navigates with ArrowDown and selects with Enter', async () => {
-    const handleChange = vi.fn()
-    const handleSelect = vi.fn()
+  it('return the old value with ArrowLeft', () => {
+    const { handleChange } = renderMovieSearchBar('Clube')
 
-    render(
-      <MovieSearchBar
-        value="Star"
-        onChange={handleChange}
-        onSelect={handleSelect}
-        suggestions={suggestions}
-      />
-    )
+    const input = screen.getByRole('textbox')
+
+    fireEvent.keyDown(input, { key: 'ArrowDown' })
+    fireEvent.keyDown(input, { key: 'ArrowRight' })
+
+    expect(handleChange).toHaveBeenCalledWith('Clube da Luta')
+
+    fireEvent.keyDown(input, { key: 'ArrowLeft' })
+
+    expect(handleChange).toHaveBeenCalledWith('Clube')
+  })
+
+  it('navigates with ArrowDown and selects with Enter', async () => {
+    const { handleSelect, suggestions } = renderMovieSearchBar('Clube')
 
     const input = screen.getByRole('textbox')
     fireEvent.keyDown(input, { key: 'ArrowDown' })
     fireEvent.keyDown(input, { key: 'Enter' })
 
     expect(handleSelect).toHaveBeenCalledWith(suggestions[0])
+  })
+
+  it('hides highlight and blurs on pressing Escape', () => {
+    renderMovieSearchBar('Clube')
+
+    const input = screen.getByRole('textbox')
+
+    fireEvent.focus(input)
+    fireEvent.keyDown(input, { key: 'ArrowDown' })
+    fireEvent.keyDown(input, { key: 'Escape' })
+
+    expect(screen.getByText(/da Luta - Utilize a tecla →/)).toBeInTheDocument()
+
+    const highlighted = screen.queryByRole('option', { selected: true })
+    expect(highlighted).not.toBeInTheDocument()
+
+    expect(input).toBeInTheDocument()
   })
 })
